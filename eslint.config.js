24dd3558672f6886fe62e296f864497ccc1ac14a
Till Krüss <till@kruss.io>
2026-05-05 19:04:42 -0700

    upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+const stylistic = require('@stylistic/eslint-plugin');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['dist/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@stylistic': stylistic,
+    },
+    rules: {
+      ...tseslint.configs['eslint-recommended'].overrides[0].rules,
+      ...tseslint.configs.recommended.rules,
+      'quotes': ['warn', 'single'],
+      'indent': ['warn', 2, { SwitchCase: 1 }],
+      'semi': 'off',
+      'comma-dangle': ['warn', 'always-multiline'],
+      'dot-notation': 'off',
+      'eqeqeq': 'warn',
+      'curly': ['warn', 'all'],
+      'brace-style': ['warn'],
+      'prefer-arrow-callback': ['warn'],
+      'max-len': ['warn', 140],
+      'no-console': ['warn'],
+      'no-non-null-assertion': 'off',
+      'comma-spacing': ['error'],
+      'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
+      'no-trailing-spaces': ['warn'],
+      'no-useless-assignment': 'off',
+      'lines-between-class-members': ['warn', 'always', { exceptAfterSingleLine: true }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@stylistic/semi': ['warn'],
+      '@stylistic/member-delimiter-style': ['warn'],
+    },
+  },
+];
